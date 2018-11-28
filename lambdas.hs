56eaeca589d07bd7f1b680120e51e5907dfb91a1
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-11-27 22:05:54 -0800

    Update lambdas.hs

--- lambdas.hs
+++ lambdas.hs
@@ -4,14 +4,10 @@
 import Data.List
 import Data.Ord
 
-a = [ 1, 2, 3, 4, 5 ]
-b = [ "bear", "cat", "elephant", "mouse" ]
-lsort :: [[a]] -> [[a]]
-lsort = sortOn length -- lsort = sortBy (comparing length)
-
 main :: IO ()
 main =  do
+        let a = [ 1, 2, 3, 4, 5 ]
+        let b = [ "bear", "cat", "elephant", "mouse" ]
         print $ foldr (+) 0 a
         print $ foldr (*) 1 a
-        let c = lsort b
-        print c
+        print $ sortOn length b
