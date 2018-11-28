c16fb4bfc20fa74fcfb1148c89da1f7750a08fc8
Conor Hoekstra <36027403+codereport@users.noreply.github.com>
2018-11-27 22:39:35 -0800

    Update lambdas.java

--- lambdas.java
+++ lambdas.java
@@ -1,34 +1,16 @@
 // code_report
 // 
 
-public class Main {
-    
-    private static void print(List<String> a) {
-        for (String s : a) System.out.print(s + ' ');
-        System.out.println();
-    }
-    
-    private static void sort_lambda() {
-        List<String> arr = Arrays.asList("bear", "cat", "elephant", "mouse");
-        arr.sort((a, b) -> Integer.compare(a.length(), b.length()));
-        print(arr);        
-    }
+import java.util.Arrays;
+import java.util.List;
 
-    private static void add_lambda() {
-        List<Integer> arr = Arrays.asList( 1, 2, 3, 4, 5 );
-        int x = arr.stream().reduce(0, (a, b) -> a + b);
-        System.out.println(x);
-    }
-    
-    private static void product_lambda() {
-        List<Integer> arr = Arrays.asList( 1, 2, 3, 4, 5 );
-        int x = arr.stream().reduce(1, (a, b) -> a * b);
-        System.out.println(x);
-    }
-    
+public class Main { 
     public static void main(String args[]) {
-        add_lambda();
-        product_lambda();
-        sort_lambda();
+        List<String>  x = Arrays.asList("bear", "cat", "elephant", "mouse");
+        List<Integer> y = Arrays.asList( 1, 2, 3, 4, 5 );
+        x.sort((a, b) -> Integer.compare(a.length(), b.length()));
+        System.out.println(x);      
+        System.out.println(y.stream().reduce(0, (a, b) -> a + b));
+        System.out.println(y.stream().reduce(1, (a, b) -> a * b));
     }
 }
